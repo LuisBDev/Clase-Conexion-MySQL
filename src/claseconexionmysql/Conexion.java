11331d57014bd33dba0e9766c9d76e889e798cf3
LuisBalarezo <luis.balarezo@unmsm.edu.pe>
2022-12-05 11:38:05 -0500

    update

--- src/claseconexionmysql/Conexion.java
+++ src/claseconexionmysql/Conexion.java
@@ -3,6 +3,30 @@ import java.sql.*;
 
 
 public class Conexion {
+    
+    String driver = "com.mysql.cj.jdbc.Driver";
+    String url = "jdbc:mysql://localhost:3306/ejemplo";
+    //Driver, protocolo, detalles de la conexion
+    
+    String username = "root";
+    String password = "root";
+    
+    Connection conn = null;
+    
+    public Connection conectar()
+    {
+        try
+        {
+            conn = DriverManager.getConnection(url, username, password);
+            System.out.println("Conectado exitosamente!");
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+        }
+        
+        return conn;
+    }
 
     
 }
