5bf285caf6e9d21679db4e59ccc239eb9313db0b
LuisBalarezo <luis.balarezo@unmsm.edu.pe>
2022-12-05 12:05:20 -0500

    update

--- src/claseconexionmysql/ClaseConexionMySQL.java
+++ src/claseconexionmysql/ClaseConexionMySQL.java
@@ -12,8 +12,39 @@ public class ClaseConexionMySQL {
         conn = miConexion.conectar();
     }
     
+    public static void guardarDatos(String nombres, String apellidos, String codigo)
+    {
+        PreparedStatement prepStatement = null;
+        
+        try
+        {
+            String query = "INSERT INTO ejemplo VALUES(null,?,?,?)";
+            prepStatement = conn.prepareStatement(query);
+            prepStatement.setString(1,nombres);
+            prepStatement.setString(2,apellidos);
+            prepStatement.setString(3,codigo);
+            prepStatement.executeUpdate();
+            System.out.println("Registro agregado correctamente.");
+        }
+        catch(SQLException e)
+        {
+            e.printStackTrace();
+        }
+    }
+    
     public static void main(String[] args) 
     {
         conectarBaseDeDatos();
+        Scanner read = new Scanner(System.in);
+        
+        String nombre;
+        String apellido;
+        String codigo;
+        
+        nombre = read.next();
+        apellido = read.next();
+        codigo = read.next();
+        
+        guardarDatos(nombre, apellido, codigo);
     }
 }
